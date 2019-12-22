6c2433cd08b1a232e70e6900868c68e70e612ea2
Markus T <markus.talka@gmail.com>
2019-12-22 23:44:30 +0200

    One more try to fix the mobile styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,27 @@
-import React from 'react';
-import './common.scss';
+import React from "react";
+import "./common.scss";
 import { Container, Row, Col } from "react-bootstrap";
 import PersonalCard from "./components/PersonalCard";
 import DraggableSection from "./components/DraggableSection";
-import LinkList from "./components/LinkList"
-import PageTitle from './components/PageTitle';
+import LinkList from "./components/LinkList";
+import PageTitle from "./components/PageTitle";
 import { description } from "./data";
 
 function App() {
   return (
     <Container fluid>
       <Row noGutters>
-        <Col xs={12} md={{ span: 10, offset: 1 }} lg={{ span: 8, offset: 2 }} xl={{ span: 4, offset: 4 }}>
+        <Col
+          xs={12}
+          md={{ span: 10, offset: 1 }}
+          lg={{ span: 8, offset: 2 }}
+          xl={{ span: 4, offset: 4 }}
+        >
           <div className="content-holder">
             <PageTitle />
-            <PersonalCard
-              title="About me"
-              content={description}
-            />
+            <PersonalCard title="About me" content={description} />
+            <PersonalCard title="Contact" content={<LinkList />} />
             <DraggableSection></DraggableSection>
-            <PersonalCard
-              title="Contact"
-              content={<LinkList />}
-            />
           </div>
         </Col>
       </Row>
