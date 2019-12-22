348f5fd1f018edd0af5b9cc598f0dbdb4e22f108
Markus T <markus.talka@gmail.com>
2019-12-22 22:46:24 +0200

    Styling fixes and streamlined experience

--- src/components/DraggableSection.jsx
+++ src/components/DraggableSection.jsx
@@ -3,57 +3,59 @@ import ReactDOM from "react-dom";
 import PersonalCard from "./PersonalCard";
 import * as dragula from "react-dragula";
 import { ExperienceType } from "../types";
-import ExpList from "./ExpList"
+import ExpList from "./ExpList";
 // eslint-disable-next-line
-import styles from 'dragula/dist/dragula.css';
+import styles from "dragula/dist/dragula.css";
 
 export class DraggableSection extends React.Component {
+  render() {
+    return (
+      <div className="draggable-list">
+        <div className="draggablecontainer">
+          <PersonalCard
+            isDraggable
+            title="Work"
+            content={<ExpList xpType={ExperienceType.Work}></ExpList>}
+          />
+        </div>
+        <div className="draggablecontainer">
+          <PersonalCard
+            isDraggable
+            title="School"
+            content={<ExpList xpType={ExperienceType.School}></ExpList>}
+          />
+        </div>
+        <div className="draggablecontainer">
+          <PersonalCard
+            isDraggable
+            title="Certicates and courses"
+            content={<ExpList xpType={ExperienceType.Course}></ExpList>}
+          />
+        </div>
+        <div className="draggablecontainer">
+          <PersonalCard
+            isDraggable
+            title="Projects"
+            content={<ExpList xpType={ExperienceType.Project}></ExpList>}
+          />
+        </div>
+        <div className="draggablecontainer">
+          <PersonalCard
+            isDraggable
+            title="Hobbies"
+            content={<ExpList xpType={ExperienceType.Hobby}></ExpList>}
+          />
+        </div>
+      </div>
+    );
+  }
 
-    render() {
-        return (
-            <div className="draggable-list">
-                <div className="draggablecontainer">
-                    <PersonalCard
-                        isDraggable
-                        title="Work"
-                        content={<ExpList xpType={ExperienceType.Work}></ExpList>}
-                    />
-                </div>
-                <div className="draggablecontainer">
-                    <PersonalCard
-                        isDraggable
-                        title="Certicates and courses"
-                        content={<ExpList xpType={ExperienceType.Course}></ExpList>} />
-                </div>
-                <div className="draggablecontainer">
-                    <PersonalCard
-                        isDraggable
-                        title="Hobbies"
-                        content={<ExpList xpType={ExperienceType.Hobby}></ExpList>}
-                    />
-                </div>
-                <div className="draggablecontainer">
-                    <PersonalCard
-                        isDraggable
-                        title="School"
-                        content={<ExpList xpType={ExperienceType.School}></ExpList>}
-                    />
-                </div>
-                <div className="draggablecontainer">
-                    <PersonalCard
-                        isDraggable
-                        title="Projects"
-                        content={<ExpList xpType={ExperienceType.Project}></ExpList>}
-                    />
-                </div>
-            </div>
-        )
-    }
-
-    componentDidMount() {
-        var draggablecontainer = ReactDOM.findDOMNode(this);
-        dragula([draggablecontainer]);
-    }
+  componentDidMount() {
+    var draggablecontainer = ReactDOM.findDOMNode(this);
+    dragula([draggablecontainer], {
+      revertOnSpill: true
+    });
+  }
 }
 
 export default DraggableSection;
